d5a7e93f681bc811104d43e0611d2039fd0c0f02
vilmor <giraleksey@gmail.com>
2021-07-04 21:02:31 +0300

    Fixing

--- skills.js
+++ skills.js
@@ -1,3 +1,25 @@
+// fetch('db/skills.json')
+//     .then(data => data.json())
+//     .then(skills => {
+//         const dl = document.createElement('dl');
+//         dl.classList.add('skills-list');
+//         skills.forEach(item => {
+//             const dt = document.createElement('dt');
+//             dt.textContent = item.name;
+//             dt.classList.add(item.css_class);
+//             const dd = document.createElement('dd');
+//             dd.classList.add('level');
+//             const div = document.createElement('div');
+//             div.style.width = item.level + '%';
+//             div.textContent = item.level;
+//             dd.append(div);
+//             dl.append(dt);
+//             dl.append(dd);
+//         });
+//         document.querySelector('#skills').append(dl);
+//     })
+//     .catch(() => console.error("Sh'ol by ty otsuda"));
+
 fetch('db/skills.json')
     .then(data => data.json())
     .then(skills => {
@@ -12,10 +34,11 @@ fetch('db/skills.json')
             const div = document.createElement('div');
             div.style.width = item.level + '%';
             div.textContent = item.level;
-            dd.appendChild(div);
+            dd.append(div);
             dl.append(dt);
             dl.append(dd);
         });
         document.querySelector('#skills').append(dl);
     })
     .catch(() => console.error("Sh'ol by ty otsuda"));
+    
